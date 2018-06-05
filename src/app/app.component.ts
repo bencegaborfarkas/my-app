c3395713819a2f61b5f1171dc308295b21e80c7b
Farkas Bence Gabor <bencegaborfarkas@gmail.com>
2018-06-05 15:48:53 +0200

    team manager page func: done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit() {
+    // init localstorage
+    localStorage.setItem("teams", JSON.stringify([
+      {
+        id: "1",
+        name: "team1"
+      },
+      {
+        id: "2",
+        name: "team2"
+      },
+      {
+        id: "3",
+        name: "team3"
+      }
+    ]));
+    localStorage.setItem("players", []);
+    localStorage.setItem("teamCounter", 3);
+    localStorage.setItem("playerCounter", 3);
+  }
+
 }
---
+++ src/app/teams/teams.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, Input, Select } from '@angular/core';
+
+@Component({
+  selector: 'app-teams',
+  templateUrl: './teams.component.html',
+  styleUrls: ['./teams.component.css']
+})
+export class TeamsComponent implements OnInit {
+
+  teams: any[];
+  selectedTeam: any;
+
+  createTeam = function(teamName: string) {
+
+    // add new team to array
+    this.teams.push({
+      name: teamName
+    });
+
+    // remove teams from localstorage
+    localStorage.removeItem('teams');
+
+    // add updated teams to localstorage
+    localStorage.setItem('teams', JSON.stringify(this.teams));
+
+    // test
+    /* let tmp = JSON.parse(localStorage.getItem('teams'));
+    console.log(tmp);*/
+
+    // close modal
+    $('#createTeamModal').modal('hide');
+
+  };
+
+
+  updateTeam = function(selectedTeam: any, teamName: string) {
+
+    // update team name
+    selectedTeam.name = teamName;
+
+    // remove teams from localstorage
+    localStorage.removeItem('teams');
+
+    // add updated teams to localstorage
+    localStorage.setItem('teams', JSON.stringify(this.teams));
+
+    // test
+    /* let tmp = JSON.parse(localStorage.getItem('teams'));
+    console.log(tmp);*/
+
+    // close modal
+    $('#updateTeamModal').modal('hide');
+
+  };
+
+  deleteTeam = function(selectedTeam: any) {
+
+    if(confirm("Are you sure to delete "+ selectedTeam.name)) {
+      // find idx
+      var idx = this.teams.findIndex((team) => {
+        return team.id === selectedTeam.id;
+      });
+
+      console.log(idx);
+
+      // delete object
+      this.teams.splice(idx, 1);
+
+      // remove teams from localstorage
+      localStorage.removeItem('teams');
+
+      // add updated teams to localstorage
+      localStorage.setItem('teams', JSON.stringify(this.teams));
+    }
+
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    this.teams = JSON.parse(localStorage.getItem('teams'));
+    console.log(this.teams);
+
+  }
+
+}
